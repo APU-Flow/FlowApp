0d967a814f5e119fd8b118837137eabb272e943f
Jonathan Ming <jming422@gmail.com>
2017-01-30 19:04:35 -0800

    Basic form validation, KeyboardAwareScrollView Added very basic form validation on all input fields. Also switched top-level View to a KeyboardAwareScrollView.

--- scenes/register.js
+++ scenes/register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, ScrollView, TextInput, Button, Text } from 'react-native';
+import { StyleSheet, TextInput, Button, Text } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 export default class Register extends Component {
   static get defaultProps() {
@@ -21,15 +22,16 @@ export default class Register extends Component {
       city: '',
       state: '',
       zip: '',
-      submitReport: ''
+      submitReport: []
     }
 
-    this.verifyInput= this.verifyInput.bind(this);
+    this.verifyInput = this.verifyInput.bind(this);
+    this.submitToServer = this.submitToServer.bind(this);
   }
 
   render() {
     return (
-      <ScrollView style={styles.container}>
+      <KeyboardAwareScrollView keyboardDismissMode="interactive" style={styles.container}>
         <TextInput style={styles.field}
           placeholder="First Name:"
           autoCapitalize="words"
@@ -49,7 +51,7 @@ export default class Register extends Component {
           keyboardType="email-address"
           autoCapitalize="none"
           returnKeyType="next"
-          onChangeText={(text) => this.verifyInput('Email Address:', text)}
+          onChangeText={(text) => this.verifyInput('email', text)}
         />
         <Text>{this.state.email}</Text>
         <TextInput style={styles.field}
@@ -94,25 +96,68 @@ export default class Register extends Component {
           onChangeText={(text) => this.verifyInput('zip', text)}
         />
         <Text>{this.state.zip}</Text>
-        <Button title="Submit" onPress={() => {
-          this.setState({submitReport: 'Submitted! Thanks, ' + this.state.firstName + '!'});
-        }} />
+        <Button title="Submit" onPress={this.submitToServer} />
         <Text>{this.state.submitReport}</Text>
-
-        
-      </ScrollView>
+        <Text>{'\n\n'}</Text>
+        <Text>{this.state.serverResponse}</Text>
+      </KeyboardAwareScrollView>
     )
   }
 
   verifyInput(name, text) {
-   let msg = '';
-
-    if (name === 'zip') {
-      let zipPatt = /^\d{5}(-\d{4})?$/
-      msg = (zipPatt.test(text)) ? 'Nice job, good zip!\n' : 'Failure! Bad zip.\n';
+    switch(name) {
+      case 'firstName':
+        this.state.submitReport[0] = (/^[A-Z' \-]$/i.test(text)) ? 'Good firstName\n' : 'Bad firstName\n';
+        break;
+      case 'lastName':
+        this.state.submitReport[1] = (/^[A-Z'\-]$/i.test(text)) ? 'Good zlastNameip\n' : 'Bad zip\n';
+        break;
+      case 'email':
+        this.state.submitReport[2] = (/^[A-Z0-9._\-%+]+@[A-Z0-9\-.]+\.[A-Z]{2,4}$/i.test(text)) ? 'Good email\n' : 'Bad email\n';
+        break;
+      case 'password':
+        this.state.submitReport[3] = (/^[A-Z0-9`~!@#%^&*()\-=_+<>,.?]{5,20}$/i.test(text)) ? 'Good password\n' : 'Bad password\n';
+        break;
+      case 'passwordVerification':
+        this.state.submitReport[4] = (text == this.state.password) ? 'Passwords match\n' : 'Passwords do not match\n';
+        break;
+      case 'address':
+        this.state.submitReport[5] = (/^[0-9]{1,8} [A-Z'#.& \-]{2,}[^ ]$/i.test(text)) ? 'Good address\n' : 'Bad address\n';
+        break;
+      case 'city':
+        this.state.submitReport[6] = (/^[A-Z' \-]{2,}[^ ]$/i.test(text)) ? 'Good city\n' : 'Bad city\n';
+        break;
+      case 'state':
+        this.state.submitReport[7] = (/^[A-Z]{2}$/.test(text)) ? 'Good state\n' : 'Bad state\n';
+        break;
+      case 'zip':
+        this.state.submitReport[8] = (/^\d{5}(-\d{4})?$/.test(text)) ? 'Good zip\n' : 'Bad zip\n';
+        break;
     }
 
-    this.setState({submitReport: msg});
+    this.setState({[name]: text});
+  }
+
+  submitToServer() {
+    fetch('http://138.68.56.236:3000/api/newUser', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        email: this.state.email,
+        password: this.state.password,
+        streetAddress: this.state.address,
+        city: this.state.city,
+        state: this.state.state,
+        zip: this.state.zip
+      })
+    })
+    .then((response) => response.text())
+    .then((responseText) => this.setState({serverResponse: responseText}));
   }
 
 }
