c8152c9b24c0220552971e152956bea6579a8d78
Jonathan Ming <jming422@gmail.com>
2017-02-09 16:13:42 -0800

    Navigator element added to index files The Navigator-based index now gives our app a navigation stack. Currently, the Navigator loads the splash scene, which automatically transitions to the login scene after a 3 second timer.

--- index.android.js
+++ index.android.js
@@ -5,16 +5,33 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View
+  Navigator
 } from 'react-native';
 
+import Splash from './scenes/splash';
 import Login from './scenes/login/login';
 
 export default class FlowApp extends Component {
   render() {
     return (
-      <Login />
+      <Navigator
+        initialRoute={{ title: 'Flow', index: 0 }}
+        renderScene={(route, navigator) => {
+          if (route.index === 0) { 
+            return (
+              <Splash 
+                title={route.title}
+
+                // Function that's called to load login scene
+                loadNextScene={ () => navigator.push({ title: 'Log In', index: route.index + 1 }) }
+              />
+            );
+          } else {
+            return <Login />
+          }
+          
+        }}
+      />
     );
   }
 }
--- scenes/splash.js
+++ scenes/splash.js
@@ -5,28 +5,58 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default class Splash extends Component{
+  
+  // Define default loadNextScene prop - this should only be used when the <Splash> element
+  // is used improperly in our code elsewhere, but it'll be an easier error to debug this way.
+  static get defaultProps() {
+    return {
+      loadNextScene: () => this.setState({ message: 'Error loading login scene; no loadNextScene method given!' })
+    };
+  }
+
+  // Initialize message state variable so the Text field doesn't get mad. 
+  constructor(props) {
+    super(props);
+    this.state = { message: '' }
+  }
+
+  // TEMP!! Set a timer to leave the splash screen up for 3 seconds (just so we can see it / debug)
+  componentDidMount() {
+    this.state.timer = setTimeout(this.props.loadNextScene, 3000);
+  }
+  // Clear the timer if the component is unmounted before the timer is up, so the timer doesn't leak
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  
   render() {
     return (
       <View style={styles.wrapper}>
-        <Text style={styles.title}>
-          FLOW
-        </Text>
+        <Text style={styles.title}>FLOW</Text>
+        <Text style={styles.message}>{this.state.message}</Text>
       </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   wrapper: {
-    backgroundColor:'rgb(52, 152, 219)',
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
+    backgroundColor: 'rgb(52, 152, 219)',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   title: {
-    color:'white',
-    fontSize:45,
-    fontWeight:'bold',
-    paddingBottom:40
+    color: 'white',
+    fontSize: 45,
+    fontWeight: 'bold',
+    paddingBottom: 40
+  },
+  message: {
+    color: 'white',
+    fontSize: 14,
+    paddingTop: 50
   }
 });
