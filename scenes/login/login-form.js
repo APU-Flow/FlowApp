4ba2e3639835e44eaa707f7693f5b03b90b5ae25
Zach Clark <zachbclark@gmail.com>
2017-02-11 21:03:00 -0800

    Moved code from Login-Form to Login

--- scenes/login/login-form.js
+++ scenes/login/login-form.js
@@ -20,27 +20,11 @@ export default class LoginForm extends Component {
 
     this.submitToServer = this.submitToServer.bind(this);
   }
-
-  onButtonPress(){
-    this.props.navigator.push({
-      id:'Register'
-    });
-  }
   render() {
     return (
-       // {/*<Text>{this.state.submitReport}</Text>*/}
       <View>
-        <TouchableHighlight 
-            style={styles.buttonLoginContainer}>
-            <Text style={styles.buttonText}>LOGIN</Text>
-            {/*onPress={this.submitToServer} */}
-          </TouchableHighlight>
-          <TouchableHighlight 
-          onPress={this.onButtonPress.bind(this)}
-          style={styles.buttonSignUpContainer}>
-            <Text style={styles.buttonText}>SIGN UP</Text>
-          </TouchableHighlight>
-        </View>
+        {/*onPress={this.submitToServer.bind(this)}*/}
+      </View>
     );
   }
 
@@ -55,9 +39,9 @@ export default class LoginForm extends Component {
         email: this.state.email,
         password: this.state.password
       })
-    })    
-    .then((response) => response.text())
-    .then((responseText) => console.log(responseText));
+    })
+      .then((response) => response.text())
+      .then((responseText) => console.log(responseText));
   }
 }
 
@@ -65,31 +49,12 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: 'flex-start'
   },
-  // input: {
-  //   height: 40,
-  //   backgroundColor: 'rgba(255,255,255,0.2)',
-  //   // marginBottom: 10,
-  //   color: '#FFF',
-  //   paddingHorizontal: 10,
-  //   padding: 20,
-  // },
-  buttonLoginContainer: {
-    backgroundColor: 'rgb(31,58,147)',
-    paddingVertical: 15,
-    // marginTop:10,
-    height: 60,
-    justifyContent:'flex-end'
-  },
-  buttonSignUpContainer: {
-    backgroundColor: 'rgb(171,183,183)',
-    paddingVertical: 15,
-    height: 60,
-    justifyContent:'flex-end'
-  },
-  buttonText: {
-    textAlign: 'center',
+  input: {
+    height: 40,
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    // marginBottom: 10,
     color: '#FFF',
-    fontWeight: '700',
-    fontSize:20
+    paddingHorizontal: 10,
+    padding: 20,
   }
 });
--- scenes/login/login.js
+++ scenes/login/login.js
@@ -2,32 +2,48 @@
 // Flow
 
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, KeyboardAvoidingView } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableHighlight, Navigator } from 'react-native';
 import LoginForm from './login-form';
 
-export default class Login extends Component{
+export default class Login extends Component {
+
+  onButtonPress() {
+    this.props.navigator.push({
+      id: 'login-form'
+    });
+  }
+
   render() {
+    const routes = [
+      { title: 'LoginForm', index: 0 },
+      { title: 'Register', index: 1 },
+    ];
     return (
-      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <View style={styles.container}>
         <View style={styles.logoContainer}>
-          <Image
-            style={styles.logo}
+          <Image style={styles.logo}
             source={require('../images/flow.png')}
           />
         </View>
         <View>
-          <LoginForm />
+          <TouchableHighlight style={styles.buttonLoginContainer}>
+            onPress={this.onButtonPress.bind(this)}
+            <Text style={styles.buttonText}>LOGIN</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.buttonSignUpContainer}>
+            <Text style={styles.buttonText}>SIGN UP</Text>
+          </TouchableHighlight>
         </View>
-      </KeyboardAvoidingView>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     flexDirection: 'column',
-    backgroundColor:'rgb(52,152,219)'
+    backgroundColor: 'rgb(52,152,219)'
   },
   logo: {
     flex: 1,
@@ -36,9 +52,27 @@ const styles = StyleSheet.create({
     resizeMode: 'contain'
   },
   logoContainer: {
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-    flexGrow:1
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexGrow: 1
+  },
+  buttonLoginContainer: {
+    backgroundColor: 'rgb(31,58,147)',
+    paddingVertical: 15,
+    height: 60,
+    justifyContent: 'flex-end'
+  },
+  buttonSignUpContainer: {
+    backgroundColor: 'rgb(171,183,183)',
+    paddingVertical: 15,
+    height: 60,
+    justifyContent: 'flex-end'
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontWeight: '700',
+    fontSize: 20
   }
 });
