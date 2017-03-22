54145b86ef3c9f63eb617edf9b6d01d9954b3694
Zach Clark <zachbclark@gmail.com>
2017-03-21 21:53:44 -0700

    Fixed the nasty errors from unlinking native-base

--- scenes/overview.js
+++ scenes/overview.js
@@ -2,23 +2,32 @@
 // Flow
 
 import React, { Component } from 'react';
-import { AsyncStorage, StyleSheet, View, Text } from 'react-native';
+import {
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+  AlertIOS
+} from 'react-native';
 
-export default class Overview extends Component {
-  
-  static get defaultProps() {
-    return {
-      message: 'Default message'
-    };
-  }
-
-  constructor(props) {
-    super(props);
-    // Initialize state variables
-    this.state = {
-      data: ''
-    }
-  }
+export default class FirstTabScreen extends Component {
+  static navigatorButtons = {
+    leftButtons: [{
+      icon: require('../../img/navicon_menu.png'),
+      id: 'menu'
+    }],
+    rightButtons: [
+      {
+        title: 'Edit',
+        id: 'edit'
+      },
+      {
+        icon: require('../../img/navicon_add.png'),
+        id: 'add'
+      }
+    ]
+  };
 
   componentDidMount() {
     AsyncStorage.multiGet(['email', 'token'], (errors, results) => {
@@ -38,32 +47,82 @@ export default class Overview extends Component {
           'x-access-token': token
         }
       })
-      .then((response) => response.text())
-      .then((responseText) => {
-        this.setState({ data: responseText });
-      });
+        .then((response) => response.text())
+        .then((responseText) => {
+          this.setState({ data: responseText });
+        });
     });
   }
 
+
+  static navigatorStyle = {
+    drawUnderTabBar: true
+  };
+  constructor(props) {
+    super(props);
+    // if you want to listen on navigator events, set this up
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+  }
+  onNavigatorEvent(event) {
+    if (event.id == 'menu') {
+      this.props.navigator.toggleDrawer({
+        side: 'left',
+        animated: true
+      });
+    }
+    if (event.id == 'edit') {
+      AlertIOS.alert('NavBar', 'Edit button pressed');
+    }
+    if (event.id == 'add') {
+      AlertIOS.alert('NavBar', 'Add button pressed');
+    }
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>{this.props.message}</Text>
-        <Text style={styles.text}>{this.state.data}</Text>
+      <View style={{ flex: 1, padding: 20 }}>
+
+        <TouchableOpacity onPress={this.onPushPress.bind(this)}>
+          <Text style={styles.button}>Push Plain Screen</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.onPushStyledPress.bind(this)}>
+          <Text style={styles.button}>Push Styled Screen</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.onModalPress.bind(this)}>
+          <Text style={styles.button}>Show Modal Screen</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.onLightBoxPress.bind(this)}>
+          <Text style={styles.button}>Show LightBox</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.onInAppNotificationPress.bind(this)}>
+          <Text style={styles.button}>Show In-App Notification</Text>
+        </TouchableOpacity>
+
       </View>
     );
   }
-
+  onPushPress() {
+    this.props.navigator.push({
+      title: "Settings"
+    });
+  }
+  onPushStyledPress() {
+    this.props.navigator.push({
+      title: "Styled",
+      screen: "example.StyledScreen"
+    });
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    flex: 1,
-    backgroundColor:'rgb(52,152,219)'
-  },
-  text: {
-    margin: 10,
-    fontSize: 15
+  button: {
+    textAlign: 'center',
+    fontSize: 18,
+    marginBottom: 10,
+    marginTop: 10,
+    color: 'blue'
   }
 });
