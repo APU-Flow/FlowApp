162d394368d54737e65843d20871d8dd0ac6b874
Unknown <jrohweller13@apu.edu>
2017-03-27 13:13:53 -0700

    Index points to splash

--- index.android.js
+++ index.android.js
@@ -36,7 +36,7 @@ export default class FlowApp extends Component {
   render() {
     return (
       <Navigator
-        initialRoute={{ name: 'dailyBarData' }}
+        initialRoute={{ name: 'splash' }}
         configureScene={(route) => route.sceneConfig || Navigator.SceneConfigs.FloatFromBottomAndroid}
         renderScene={(route, navigator) => {
           let scene = <Text>Bad route name given!</Text>;
