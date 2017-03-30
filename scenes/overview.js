3708ced4de32b2638f92722cd41780827eca4e83
Unknown <jrohweller13@apu.edu>
2017-03-30 00:55:13 -0700

    Overview Page Styling

--- scenes/overview.js
+++ scenes/overview.js
@@ -27,6 +27,7 @@ export default class Overview extends Component {
     this.state = {
       data: ''
     };
+    this.color = 'white';
   }
 
   componentDidMount() {
@@ -38,8 +39,8 @@ export default class Overview extends Component {
       let token = results[1][1];
       // let now = new Date();
       // let hourAgo = new Date();
-      // hourAgo.setHours(hourAgo.getHours()-1);
-      fetch(`http://138.68.56.236:3000/api/getUsageEvent?email=${encodeURI(email)}`, {
+      // hourAgo.setHours(hourAgo.getHours()-1);//token, email, date, meterID=1,
+      fetch(`http://138.68.56.236:3000/api/getDailyUsage?email=${encodeURI(email)}&date=${encodeURI(new Date())}&meterID=1&token=${encodeURI(token)}`, {
         method: 'GET',
         headers: {
           'Accept': 'application/json',
@@ -47,9 +48,9 @@ export default class Overview extends Component {
           'x-access-token': token
         }
       })
-      .then((response) => response.text())
-      .then((responseText) => {
-        this.setState({ data: responseText });
+      .then((response) => response.json())
+      .then((responseObject) => {
+        this.setState({ data: responseObject.data });
       });
     });
   }
@@ -60,16 +61,16 @@ export default class Overview extends Component {
         <View>
           <Text style={styles.text}>{this.props.message}</Text>
           <Text style={styles.text}>{this.state.data}</Text>
-          <Text style={styles.title}>Today</Text>
+          <Text style={styles.title}>Overview</Text>
         </View>
          <Chart
-          color={['white']}
-          axisColor={['white']}
-          axisLabelColor={['white']}
+          color={"white"}
+          axisColor={"white"}
+          axisLabelColor={"white"}
           axisLineWidth={1}
 
-          xAxisHeight={40}
-          yAxisWidth={19}
+          xAxisHeight={20}
+          yAxisWidth={60}
 
           cornerRadius={4}
 
@@ -89,7 +90,7 @@ export default class Overview extends Component {
           showAxis={true}
                     
           style={styles.chart}
-          labelFontSize={8}                  
+          labelFontSize={11}                  
           />
       </View>
     );
@@ -97,51 +98,21 @@ export default class Overview extends Component {
 
 }
 
-
-/*Pseudo-code for getting input from database:
-Stringify JSON object 
-ex. (timeStarted,timeEnded,totalWaterUsed) 
-eg. (8.23am=>8.45am,10)
-//categorize into which hour the event was..ie if user
-//had shower from 8:59am until 9:30 then it will be graphed as a 9am event.
-//same hour
-if (charAt(1)==8 && charAt(5)==a && charAt(9)==8 && charAt(13)==a)
-{
-  arrayOfTimesAndUsage[[0,0]]=8
-  //if 2 digit number (check closed parenthesis position)
-  //then
-    arrayOfTimesAndUsage[[0,1]]=parseInt(charAt(16)&&charAt(17))
-  //else
-    arrayOfTimesAndUsage[[0,1]]=parseInt(charAt(16)
-}
-//^^copy and paste this for different hours and times (am/pm, 1-12)
-//could make life a lot easier if we just went off time ended or time started
-//or middle between them.
-//goes into next hour, choose where more water was used as hour
-//goes into multiple hours,
-//usageEvents when it goes on for more than a day?
-//add up multiple events within same hour, make one piece of data
-//dataMlUsageHr==
-//
- */
-
-
-dataMlUsageHr=[1,3,9,4,8,3,7,9,4,8,3,7,4];
+dataMlUsageHr=[0,0,0,400,8000,3543,7000,9000,4000,8000,3000,7000];
 //change this so it gets input from database
 dataOverview = [
-    ["8am", dataMlUsageHr[0]],
-    ["9am", dataMlUsageHr[1]],
-    ["10am", dataMlUsageHr[2]],
-    ["11am", dataMlUsageHr[3]],
-    ["12pm", dataMlUsageHr[4]],
-    ["1pm", dataMlUsageHr[5]],
-    ["2pm", dataMlUsageHr[6]],
-    ["3pm", dataMlUsageHr[7]],
-    ["4pm", dataMlUsageHr[8]],
-    ["5pm", dataMlUsageHr[9]],
-    ["6pm", dataMlUsageHr[10]],
-    ["7pm", dataMlUsageHr[11]],
-    ["8pm", dataMlUsageHr[12]],
+    ["8a", dataMlUsageHr[0]],
+    ["9a", dataMlUsageHr[1]],
+    ["10a", dataMlUsageHr[2]],
+    ["11a", dataMlUsageHr[3]],
+    ["12p", dataMlUsageHr[4]],
+    ["1p", dataMlUsageHr[5]],
+    ["2p", dataMlUsageHr[6]],
+    ["3p", dataMlUsageHr[7]],
+    ["4p", dataMlUsageHr[8]],
+    ["5p", dataMlUsageHr[9]],
+    ["6p", dataMlUsageHr[10]],
+    ["7p", dataMlUsageHr[11]],
 ];
 
 const styles = StyleSheet.create({
@@ -157,11 +128,11 @@ const styles = StyleSheet.create({
     fontSize: 15
   },
    chart: {
-        width: 280,
+        width: 300,
         height: 100,
         margin: 1,
         marginTop: 25,
-        marginBottom: 100,
+        marginBottom: 120,
     },
   title: 
     {
